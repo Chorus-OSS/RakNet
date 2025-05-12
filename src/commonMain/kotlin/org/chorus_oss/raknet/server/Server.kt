6263a59be2c4e3d0e3612805ad377a22c6bb1ef7
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-05-12 20:48:06 +1200

    refactor

--- src/commonMain/kotlin/org/chorus_oss/raknet/server/Server.kt
+++ src/commonMain/kotlin/org/chorus_oss/raknet/server/Server.kt
@@ -1,6 +1,5 @@
 package org.chorus_oss.raknet.server
 
-import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.network.selector.*
 import io.ktor.network.sockets.*
 import kotlinx.coroutines.*
@@ -15,13 +14,9 @@ import org.chorus_oss.raknet.types.HeaderFlags
 import org.chorus_oss.raknet.types.PacketHeader
 import kotlin.coroutines.CoroutineContext
 
-class Server : CoroutineScope {
+class Server(private val socket: BoundDatagramSocket) : CoroutineScope {
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.IO + SupervisorJob() + CoroutineName("RakNetServer")
-
-    private val selector: SelectorManager = SelectorManager(Dispatchers.IO + CoroutineName("RakNetServer - SelectorManager"))
-
-    private val bindings: MutableSet<BoundDatagramSocket> = mutableSetOf()
 
     private val connections: MutableMap<SocketAddress, Connection> = mutableMapOf()
 
@@ -36,37 +31,19 @@ class Server : CoroutineScope {
 
         alive = true
 
-        bindings.forEach {
-            launch {
-                while (alive) {
-                    handle(it.receive(), it)
-                }
+        launch {
+            while (alive) {
+                handle(socket.receive())
             }
         }
     }
 
-    suspend fun bind(ip: String, port: Int) {
-        bind(InetSocketAddress(ip, port))
-    }
-
-    suspend fun bind(address: SocketAddress) {
-        if (alive) {
-            throw IllegalStateException("Cannot bind while server is running!")
-        }
-
-        val socket = aSocket(selector)
-            .udp()
-            .bind(address)
-
-        this.bindings.add(socket)
-    }
-
-    private suspend fun handle(datagram: Datagram, from: BoundDatagramSocket) {
+    private suspend fun handle(datagram: Datagram) {
         val header = datagram.packet.peek().readUByte()
 
         val offline = header and HeaderFlags.VALID == 0u.toUByte()
         if (offline) {
-            return handleOffline(datagram, from)
+            return handleOffline(datagram)
         }
 
         val connection = connections[datagram.address]
@@ -75,7 +52,7 @@ class Server : CoroutineScope {
         }
     }
 
-    private suspend fun handleOffline(datagram: Datagram, from: BoundDatagramSocket) {
+    private suspend fun handleOffline(datagram: Datagram) {
         val header = datagram.packet.readUByte()
 
         when (header) {
@@ -97,8 +74,8 @@ class Server : CoroutineScope {
                         "RakNet Server",
                         "Survival",
                         1,
-                        from.localAddress.port(),
-                        from.localAddress.port() + 1
+                        socket.localAddress.port(),
+                        socket.localAddress.port() + 1
                     ).joinToString(separator = ";", postfix = ";")
                 )
 
@@ -106,7 +83,7 @@ class Server : CoroutineScope {
                 UnconnectedPong.serialize(pong, buffer)
 
                 val outDatagram = Datagram(packet = buffer, address = datagram.address)
-                from.send(outDatagram)
+                socket.send(outDatagram)
             }
 
             PacketHeader.OPEN_CONNECTION_REQUEST_1 -> {
@@ -116,6 +93,22 @@ class Server : CoroutineScope {
             PacketHeader.OPEN_CONNECTION_REQUEST_2 -> {
 
             }
+        }
+    }
+
+    companion object {
+        private val selector: SelectorManager = SelectorManager(Dispatchers.IO + CoroutineName("RakNetServer - SelectorManager"))
+
+        suspend fun bind(ip: String, port: Int): Server {
+            return bind(InetSocketAddress(ip, port))
+        }
+
+        suspend fun bind(address: SocketAddress): Server {
+            val socket = aSocket(selector)
+                .udp()
+                .bind(address)
+
+            return Server(socket)
         }
     }
 }
